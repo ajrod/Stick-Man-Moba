24fc921a4e8059980e47540f0076485e90a3e81c
Jeremy Adams <bobby.joe.222@hotmail.com>
2013-02-26 18:22:28 -0500

    Made player input class

---
+++ Side_Scrolling_StickMen/Side_Scrolling_StickMen/Side_Scrolling_StickMen/Side_Scrolling_StickMen/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Side_Scrolling_StickMen
+{
+    public class Player
+    {
+        public KeyboardState currentKeyboardState;
+        public KeyboardState previousKeyboardState;
+        public MouseState currentMouseState;
+        public MouseState previousMouseState;
+
+        public Player()
+        {
+
+        }
+
+        public void Update()
+        {
+            checkKeyboard();
+            checkMouse();
+        }
+
+        public void checkKeyboard()
+        {
+
+        }
+
+        public void checkMouse()
+        {
+
+        }
+    }
+}
